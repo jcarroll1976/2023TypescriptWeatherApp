1742ff81a206ded971364abbf3e3c579d1fd84bf
Joshua Carroll <carrolljosh1976@gmail.com>
2023-10-28 21:54:45 -0400

    formatted date for forecasted weather

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -7,6 +7,36 @@ interface ForecastedWeatherProps {
 
 
 function Forecast({forecastedWeather}:ForecastedWeatherProps) {
+    const formatDate = (dateString: string) => {
+        const date = new Date(dateString);
+        const day = date.getDate();
+        const monthNames = [
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        const month = monthNames[date.getMonth()];
+        const hour = date.getHours();
+        const minute = date.getMinutes();
+        const ampm = hour >= 12 ? "PM" : "AM";
+        
+        return `${hour === 0 ? 12 : (hour > 12 ? hour - 12 : hour)}${ampm} ${month} ${day}${getDaySuffix(day)}`;
+    };
+
+    const getDaySuffix = (day: number) => {
+        if (day >= 11 && day <= 13) {
+            return "th";
+        }
+        switch (day % 10) {
+            case 1:
+                return "st";
+            case 2:
+                return "nd";
+            case 3:
+                return "rd";
+            default:
+                return "th";
+        }
+    };
   return (
     <div>
         {forecastedWeather ? (
@@ -15,7 +45,7 @@ function Forecast({forecastedWeather}:ForecastedWeatherProps) {
                 <div>
                 {forecastedWeather.list.map((forecast,index)=> (
                     <div>
-                        <p key={index}>{forecast.dt_txt}</p>
+                        <p key={index}>{formatDate(forecast.dt_txt)}</p>
                         <p>{forecast.main.temp.toFixed()}</p>
                         <p><img src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`} alt='' /></p>
                     </div>
